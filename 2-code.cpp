9acd1a40a2612f15864f3c1361191b9ea96ac2ad
wScottSh <scott.sheppard@hey.com>
2022-12-12 14:54:55 -0800

    Changes to map instead of switch statement

--- 2-code.cpp
+++ 2-code.cpp
@@ -10,8 +10,54 @@
 // C Y -> lose (2 + 0) = 2
 // C Z -> draw (3 + 3) = 6
 
-// Import file
-// iterate through the file, but translate each line into the number
-    // use the above table to make a switch table [if line == 'A X' -> 4]
-// add each iteration to a variable 'score'
-// print out the final 'score'
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <utility>
+
+int main()
+{
+    // define variable to work with
+    int totalScore = 0;
+    std::map<std::string, int> scoreSheet = {
+        {"A X", 4},
+        {"A Y", 8},
+        {"A Z", 3},
+        {"B X", 1},
+        {"B Y", 5},
+        {"B Z", 9},
+        {"C X", 7},
+        {"C Y", 2},
+        {"C Z", 6},
+    };
+
+    // Import file
+    std::ifstream rounds("2-data.txt");
+    std::string line;
+    while (getline(rounds, line))
+    {
+        // Import file
+        std::ifstream rounds("2-data.txt");
+        std::string line;
+        
+        // iterate through the file, but translate each line into the number
+        while (getline(rounds, line))
+        {
+            // // Use the find method to look up the value for the current line
+            // auto it = values.find(line);
+            // if (it != values.end())
+            // {
+            //     // If the line is found in the map, add each iteration to a variable 'score'
+            //     totalScore += it->second;
+            // }
+        }
+    }
+    
+    // print out the final 'score'
+    std::cout << "test score: " << totalScore << std::endl;
+}
+
+
+
+
