d03e09068682ea564bf4c375648f7dbe83558431
wScottSh <scott.sheppard@hey.com>
2022-12-12 11:19:23 -0800

    usus AI to wet feet in cpp - solves day 1

--- 1-code.cpp
+++ 1-code.cpp
@@ -1,26 +1,36 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include <iostream>     // for printing the results
+#include <fstream>      // reads files
+#include <vector>       // lets me build a vector array
+#include <algorithm>    // brings in max_element
 
-int main() {
-    // open the file to be worked with
-    std::ifstream input("1-data.txt");
-    
-    // read the data and save it to a variable
-    std::vector<int> calories;
-    int calorie;
-    while (input >> calorie)
+using namespace std;    // makes the code more readable so I don't have to write std:: in 
+                        // front of everything
+
+int main()  // the main function that is executed by c++
+{
+    // set up my variables: a vector to store data and temporary varibles to iterate
+    vector<int> totals;     // defines a vector of integers called `totals`
+    int current_elf = 0;    // defines current_elf for iterating
+    int total = 0;          // defines total for an individual elf
+
+    ifstream data_file("1-data.txt");   // open the file
+    string line;                        // define a `line` variable for storing strings of data
+    while (getline(data_file, line))    // iterate through the file looking at each line for a string
     {
-        calories.push_back(calorie);
+        if (line.empty())               // look for a new elf via empty line separators
+        {                               // then move to the next elf
+            totals.push_back(total);    // puts the current total into the totals vector
+            total = 0;                  // resets total back to 0
+            current_elf++;              // iterates to the next elf
+        }
+        else                            // otherwise...
+        {                               // do the actual adding
+            total += stoi(line);        // convert the line into an int and add it to total
+        }
     }
+    totals.push_back(total); // add the total for the final elf
+    int max_total = *max_element(totals.begin(), totals.end()); // define the range and find the max
+    cout << "Elf " << current_elf << " carried the most calories with " << max_total << "." << endl;    // print the output
 
-    // close the input file (for garbage colletion?)
-    input.close();
-    
-    // write a function that separates the chunks newline separated chunks of data into a bunch of list/array (whatver they're called in C++)
-    
+    return 0;   // indicate the program ran successfully
 }
-
-// define the data as a variable
-// write a function that iterates through every chunk and adds them together, pushing those into a new array
-// return the highest number
