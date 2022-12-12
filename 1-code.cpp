01a46bf70bb4e688b2d5d3e2d723344e44fc6b9f
wScottSh <scott.sheppard@hey.com>
2022-12-12 07:52:49 -0800

    writes initial pseudo-code for day 1

---
+++ 1-code.cpp
@@ -0,0 +1,5 @@
+// import the data
+// define the data as a variable
+// write a function that separates the chunks newline separated chunks of data into a list/array (whatver they're called in C++)
+// write a function that iterates through every chunk and adds them together, pushing those into a new array
+// return the highest number
